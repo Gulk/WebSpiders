6ff69ed4fd5cc366d9cea94e164d68687c68caaa
Avinerry <avinerry@avinerry>
2015-11-18 20:15:18 +0100

    CHANGED: player_entry.html & main.css & playerDetails.js ADDED: data/data.json
    task4 render json in table
    
    1424631 / 1423687

--- Meilenstein 4/js/playerDetails.js
+++ Meilenstein 4/js/playerDetails.js
@@ -3,10 +3,10 @@ var request = new XMLHttpRequest();
 
 function manipuliere() {
 
-    // URL fï¿½rRequest festlegen
-    request.open("GET", "http://martinakraus.net/data.json");   // request.status = 0 ???
+    // URL fürRequest festlegen
+    request.open("GET", "../data/data.json");
 
-    // URL fï¿½rRequest festlegen
+    // URL fürRequest festlegen
     request.onreadystatechange = callbackHandler;
 
     // Request abschicken
@@ -15,37 +15,72 @@ function manipuliere() {
 }
 
 function callbackHandler() {
-
-    alert("request.readyState = " + request.readyState + "\n"
-        + "request.status = " + request.status + "\n"
-        + "request.responseText = " + request.responseText);
-
     if ((request.readyState == 4) && (request.status == 200) && (request.responseText != null)) {
-        alert(request.responseText);
-
-        var myObj = JSON.parse(request.responseText);
-        createHTMLTable(myObj);
-
+        var jSonArray = JSON.parse(request.responseText);
+        createHTMLTable(jSonArray);
     }
 }
 
-function createHTMLTable(myObj) {
-    // TODO convert Player to HTML
+function createHTMLTable(jSonArray) {
+
+    var result = "<table> <tr> <th>Spieler</th> <th>Verein</th> <th>Headcoach</th> <th>Assistantcoach</th> <th>Position</th> <th>Aktiv</th> <th>R&uumlckennummer</th> <th>Geburtsjahr</th> </tr>";
+
+    for (var i = 0; i < jSonArray.length; i++) {
+        result += "<tr>";
+        result += "<td>" + jSonArray[i].surname + "</td>";
+        result += "<td>" + jSonArray[i].team + "</td>";
+        result += "<td>" + jSonArray[i].headcoach + "</td>";
+        result += "<td>" + jSonArray[i].asisstantcoach + "</td>";
+        result += "<td>" + jSonArray[i].position + "</td>";
+
+        if (jSonArray[i].isActive) {
+            result += "<td>" + "Ja" + "</td>";
+        } else {
+            result += "<td>" + "Nein" + "</td>";
+        }
+
+
+        result += "<td>" + jSonArray[i].number + "</td>";
+        result += "<td>" + jSonArray[i].year + "</td>";
+        result += "</tr>";
+    }
+
+    result += "</table>";
+
+    document.getElementById('jsTable').innerHTML = result;
+
+
 }
 
-function updateTable(source){
-    if(source === "all"){
+function createHTMLTableFavOnly(jSonArray) {
+
+    var FavOnly = new Array();
+    var favCounter = 0;
+
+    for (var i = 0; i < jSonArray.length; i++) {
+        if (jSonArray[i].isFavorite) {
+            FavOnly[favCounter] = jSonArray[i];
+            favCounter++;
+        }
+    }
+
+    createHTMLTable(FavOnly);
+
+}
+
+function updateTable(source) {
+    if (source === "all") {
         var allPlayers = document.getElementById("allplayers");
         var nameOfClass = allPlayers.className;
         var classArray = nameOfClass.split(" ");
-        if(classArray.length == 1){
+        if (classArray.length == 1) {
             allPlayers.className += "selected";
             alert(nameOfClass);
-        }else{
+        } else {
 
         }
 
-    }else{
+    } else {
         var myfavourite = document.getElementById("myfav");
     }
 
