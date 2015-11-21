330ad9021aa41613bdd2f7995977e90244441dc2
Gulk <tsir_senpai@web.de>
2015-11-21 17:14:34 +0100

    CHANGED playerDetails.js
    Is now the correct table and requests function js file. Made a few clean
    coding adjustments
    task 4
    1423687

--- Meilenstein 4/js/playerDetails.js
+++ Meilenstein 4/js/playerDetails.js
@@ -1,94 +1,77 @@
 var request = new XMLHttpRequest();
+var jSonArray;
 
-
-function manipuliere() {
-
-    // URL fürRequest festlegen
-    request.open("GET", "../data/data.json");
-
-    // URL fürRequest festlegen
+function loadTable() {
+    request.open("GET", "../data/data.json", true);
     request.onreadystatechange = callbackHandler;
-
-    // Request abschicken
     request.send();
-
 }
 
 function callbackHandler() {
     if ((request.readyState == 4) && (request.status == 200) && (request.responseText != null)) {
-        var jSonArray = JSON.parse(request.responseText);
-        createHTMLTable(jSonArray);
+        jSonArray = JSON.parse(request.responseText);
+        populateTable();
     }
 }
 
-//<script type="text/javascript" language="JavaScript" src="../js/playerDetails.js"></script>
-/*<script type="text/javascript">
-    manipuliere();
-</script>
-    */
-
-function createHTMLTable(jSonArray) {
-
-    var result = "<table> <tr> <th>Spieler</th> <th>Verein</th> <th>Headcoach</th> <th>Assistantcoach</th> <th>Position</th> <th>Aktiv</th> <th>R&uumlckennummer</th> <th>Geburtsjahr</th> </tr>";
+function populateTable() {
 
     for (var i = 0; i < jSonArray.length; i++) {
-        result += "<tr>";
-        result += "<td>" + jSonArray[i].surname + "</td>";
-        result += "<td>" + jSonArray[i].team + "</td>";
-        result += "<td>" + jSonArray[i].headcoach + "</td>";
-        result += "<td>" + jSonArray[i].asisstantcoach + "</td>";
-        result += "<td>" + jSonArray[i].position + "</td>";
+        var table = document.getElementById("playertable");
+        var row = table.insertRow(i + 1);
 
-        if (jSonArray[i].isActive) {
-            result += "<td>" + "Ja" + "</td>";
-        } else {
-            result += "<td>" + "Nein" + "</td>";
-        }
-
-
-        result += "<td>" + jSonArray[i].number + "</td>";
-        result += "<td>" + jSonArray[i].year + "</td>";
-        result += "</tr>";
+        row.insertCell(0).innerHTML = jSonArray[i].firstname + " " + jSonArray[i].surname;
+        row.insertCell(1).innerHTML = jSonArray[i].team;
+        row.insertCell(2).innerHTML = jSonArray[i].headcoach;
+        row.insertCell(3).innerHTML = jSonArray[i].asisstantcoach;
+        row.insertCell(4).innerHTML = jSonArray[i].position;
+        row.insertCell(5).innerHTML = booleanToTextConverter(jSonArray[i].isActive);
+        row.insertCell(6).innerHTML = jSonArray[i].number;
+        row.insertCell(7).innerHTML = jSonArray[i].year;
     }
-
-    result += "</table>";
-
-    document.getElementById('jsTable').innerHTML = result;
-
-
 }
 
-function createHTMLTableFavOnly(jSonArray) {
-
-    var FavOnly = new Array();
-    var favCounter = 0;
-
-    for (var i = 0; i < jSonArray.length; i++) {
-        if (jSonArray[i].isFavorite) {
-            FavOnly[favCounter] = jSonArray[i];
-            favCounter++;
-        }
-    }
-
-    createHTMLTable(FavOnly);
-
-}
-
-function updateTable(source) {
-    if (source === "all") {
-        var allPlayers = document.getElementById("allplayers");
-        var nameOfClass = allPlayers.className;
-        var classArray = nameOfClass.split(" ");
-        if (classArray.length == 1) {
-            allPlayers.className += "selected";
-            alert(nameOfClass);
-        } else {
-
-        }
-
+function booleanToTextConverter(isTrue) {
+    if (isTrue) {
+        return "Ja";
     } else {
-        var myfavourite = document.getElementById("myfav");
+        return "Nein";
     }
+}
+
+function showAllPlayer() {
+    selectAllPlayer();
+    renderFavOnly(false);
 
 }
 
+function showFavPlayer() {
+    selectFav();
+    renderFavOnly(true);
+
+}
+
+function selectFav() {
+    document.getElementById("myfav").classList.add("selected");
+    document.getElementById("allplayers").classList.remove("selected");
+}
+
+function selectAllPlayer() {
+    document.getElementById("allplayers").classList.add("selected");
+    document.getElementById("myfav").classList.remove("selected");
+}
+
+
+function renderFavOnly(favOnly) {
+    var rows = document.getElementById("playertable").rows;
+
+    for (var i = 0; i <= jSonArray.length; i++) {
+        if (!jSonArray[i].isFavorite) {
+            if (favOnly) {
+                rows[i + 1].style.display = 'none';
+            } else {
+                rows[i + 1].style.display = '';
+            }
+        }
+    }
+}
