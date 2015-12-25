1e89f3460c369f79a5c26170255c1d196a170237
Avinerry <avinerry@avinerry>
2015-12-25 17:39:34 +0100

    Meilenstein 5, Aufgabe 2
    UPDATED player_entry.html -- changed get to post, change action url
    ADD webserver.js -- receives data and writes into player.txt
    ADD player.txt
    
    task 5.2
    1424631 / 1423687

---
+++ Meilenstein 5/aufgabe02/backend/webserver.js
@@ -0,0 +1,41 @@
+var http = require('http');
+var qs = require('querystring');
+var fs = require('fs');
+
+
+const hostname = '127.0.0.1';
+const port = 1337;
+
+
+http.createServer(function (req, res) {
+
+    if (req.method == 'POST') {
+        var body = '';
+
+        req.on('data', function (data) {
+            body += data;
+        });
+
+        req.on('end', function () {
+
+            var post = qs.parse(body);
+            var writeToFile = post.vorname + ' ' + post.name + ', ' + post.jahr + ', ' + post.hcoach + ', ' +
+                post.acoach + ', ' + post.position + ", " + post.number + '\n';
+
+
+            fs.appendFile('players.txt', writeToFile, function (err) {
+                if (err) throw err;
+            });
+
+
+            res.writeHead(200, {'Content-Type': 'text/plain'});
+            res.end("POST was successful");
+        });
+
+    } else {
+        res.writeHead(405, {'Content-Type': 'text/plain'});
+        res.end("Method Not Allowed");
+    }
+
+
+}).listen(port, hostname);
